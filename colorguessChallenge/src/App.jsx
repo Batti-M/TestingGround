6f2f04e2226fc3cd7cc13fe81e4e4e40adad4890
Batti-M <bartosz-m@outlook.de>
2022-11-10 17:32:08 +0100

    upload of new coding challenge

---
+++ colorguessChallenge/src/App.jsx
@@ -0,0 +1,36 @@
+import { useEffect,useState } from 'react'
+
+
+function App() {
+  const [fieldColor, setFieldColor] = useState("#151515")
+  const [rightChoice,setRightChoice] = useState(false)
+  const [nextRound,setNextRound] = useState(0)
+
+  const getRandomHexColor = () => `#${Math.floor(Math.random()*0xffffff).toString(16).padEnd(6, "0")}`
+  const newColor = getRandomHexColor()
+  
+  useEffect( () => {
+    setFieldColor(newColor)
+    setRightChoice(newColor)
+  },[nextRound])
+  const possibleAnswers = [rightChoice,getRandomHexColor(),getRandomHexColor()]
+
+  const checkIfCorrect = (e) => {
+    e.target.innerHTML === fieldColor ? console.log("right Answer") : console.log("wrong answer")
+      
+  }
+  return (
+    <div className="App">
+      <div className='colorfield' onClick={() => setNextRound(prev => prev+1)} style={{backgroundColor:`${fieldColor}`}}><span>{fieldColor}</span></div>
+      <div className='buttonWrapper'>
+        <button onClick={(e) => checkIfCorrect(e)}>{possibleAnswers[0]}</button>
+        <button onClick={(e) => checkIfCorrect(e)}>{possibleAnswers[1]}</button>
+        <button onClick={(e) => checkIfCorrect(e)}>{possibleAnswers[2]}</button>
+      </div>  
+    </div>
+  )
+}
+   
+    
+
+export default App
